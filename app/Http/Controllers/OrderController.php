4b59bb07648e3bf03fc3e5a83f6eef7562459eb2
Zhardy <zhardydev@gmail.com>
2024-09-30 17:06:42 +0800

    feat: add review model

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Payment;
 use App\Models\Product;
+use App\Models\Review;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
