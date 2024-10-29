26914301b792896da385493ca6af7d0860f9476d
Zhardy <zhardydev@gmail.com>
2024-10-29 15:57:04 +0800

    feat: makes data become nullable to prevent null error

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('role');
-            $table->string('address');
-            $table->integer('phone_number');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('role')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('phone_number')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
