89b5a9e5b7a985e2f582feb04ace0ed124f97f77
Zhardy <zhardydev@gmail.com>
2024-10-30 14:51:50 +0800

    feat: add chart.js stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
     <title>Online Marketplace System</title>
 
+    <!-- add chart.js  -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
